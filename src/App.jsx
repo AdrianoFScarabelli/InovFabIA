6ab9bb8081eb1b5fdb45ee0f86826b9bbf90d713
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2025-04-10 22:30:36 -0300

    Adicionando arquivos

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import Relogio from './Relogio';
+import './App.css';
+
+const App = () => {
+  const imagens = [
+    '/foto1.jpg',
+    '/foto2.jpg',
+    '/foto3.jpg',
+  ];
+
+  const [indexAtual, setIndexAtual] = useState(0);
+
+  useEffect(() => {
+    const intervalo = setInterval(() => {
+      setIndexAtual((prevIndex) =>
+        prevIndex === imagens.length - 1 ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(intervalo);
+  }, [imagens.length]);
+
+  return (
+    <div>
+      <div className="relogio-container">
+        <Relogio/>
+      </div>
+      <div className="slider-container">
+        <img
+          src={imagens[indexAtual]}
+          alt="Imagem rotativa"
+          className="slider-image"
+        />
+      </div>
+      <div className="welcome-container">
+        <h1 className="welcome">Bem vindo, faça uma pergunta ao InovFabIA para iniciar.</h1>
+      </div>
+    </div>
+    
+  );
+};
+
+export default App;
