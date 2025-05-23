112d6da2e3f535132e3dea5433fc3dd4a44472ca
Adriano Scarabelli <adrianoscarabelli@gmail.com>
2025-05-23 16:31:09 -0300

    Primeira tentativa de conexão com a API, enviando arquivo .txt

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -6,14 +6,43 @@ import './Chat.css';
 const Chat = () => {
   const gifs = ['/escutando.gif', '/processando.gif', '/respondendo.gif'];
   const [gifIndex, setGifIndex] = useState(0);
+  const [pergunta, setPergunta] = useState('O que é o laboratório InovFabLab?');
+  const [resposta, setResposta] = useState('');
+
   const navigate = useNavigate();
 
   const handleLogoClick = () => {
     navigate('/');
-  }; 
+  };
+
+  const enviarPergunta = async () => {
+  setGifIndex(1);
+
+  try {
+    const blob = new Blob([pergunta], { type: 'text/plain' });
+    const file = new File([blob], 'pergunta.txt');
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await fetch('https://kaue-api-llm-d9dpbadvbhfaezd0.brazilsouth-01.azurewebsites.net/speech/process-file/', {
+      method: 'POST',
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    setResposta(data || 'Sem resposta da API');
+    setGifIndex(2);
+  } catch (error) {
+    setResposta('Erro ao conectar com a API');
+    setGifIndex(0);
+    console.error(error);
+  }
+  };
 
   const handleGifClick = () => {
-    setGifIndex((prevIndex) => (prevIndex + 1) % gifs.length);
+    enviarPergunta();
   };
 
   return (
@@ -27,7 +56,7 @@ const Chat = () => {
           style={{ cursor: 'pointer' }}
         />
       </div>
-      
+
       <div className="relogio-container">
         <Relogio />
       </div>
@@ -37,7 +66,10 @@ const Chat = () => {
       </div>
 
       <div className="chat-container">
-        <p className='perguntas'>O que é o laboratório InovFabLab?</p>
+        <p className='perguntas'>{pergunta}</p>
+        {resposta && (
+          <p className="resposta">{resposta}</p>
+        )}
       </div>
 
       <div className="resposta-container">
